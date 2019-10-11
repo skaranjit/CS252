182e0eefd5fe00d17f9d38db92b05c5819ce6a25
skaranjit <47037059+skaranjit@users.noreply.github.com>
2019-10-10 20:18:54 -0500

    Create integerManipulation.cpp

---
+++ integerManipulation.cpp
@@ -0,0 +1,76 @@
+#include "integerManipulation.h"
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+integerManipulation::integerManipulation(long long n){
+    num =n;
+    reverseNum();
+}
+void integerManipulation::setNum(long long n){
+    num =n;
+}
+long long integerManipulation::getNum(){
+    return num;
+}
+void integerManipulation::reverseNum(){
+    string n;
+    int rev;
+    stringstream ss;
+    ss<<num;
+    ss>>n;
+    int len = n.length();
+    for (signed int i=0;i<len;i++){
+        swap(n[i],n[len-i-1]);
+    }
+    ss.clear();
+    ss<<n;
+    ss>> rev;
+    revNum = rev;
+}
+void integerManipulation::classifyDigits(){
+    string n;
+    stringstream ss;
+    ss<<num;
+    ss>>n;
+    ss.clear();
+    for (unsigned int i=0;i<n.length();i++){
+        int x;
+        ss << n[i];
+        ss >> x;
+        ss.clear();
+        if (x == 0){
+            zerosCount++;
+
+        }
+        else if(x % 2 == 0) evensCount++;
+        else oddsCount++;  
+    }
+
+}
+int integerManipulation::getOddsCount(){
+    return oddsCount;
+}
+int integerManipulation::getEvensCount(){
+    return evensCount;
+}
+int integerManipulation::getZerosCount(){
+    return zerosCount;
+}
+int integerManipulation::sumDigits(){
+    string n;
+    stringstream ss;
+    ss<<num;
+    ss>>n;
+    ss.clear();
+    int sum;
+    for (unsigned int i=0;i<n.length();i++){
+        int x;
+        ss << n[i];
+        ss >> x;
+        ss.clear();
+        sum += x;
+}
+    return sum;
+}
